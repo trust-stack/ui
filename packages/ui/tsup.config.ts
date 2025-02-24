dacd67922b540d6f43ab447c4baefbe5ec04d21f
Harley Thomas <harleyjackthomas@gmail.com>
2025-02-25 09:08:47 +1030

    Adding publish command.

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,9 +1,13 @@
 import {defineConfig} from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.tsx"],
+  entry: ["src/**/*.ts?(x)", "!src/index.tsx"],
   format: ["esm", "cjs"],
   dts: true,
   sourcemap: true,
+  splitting: true,
+  outDir: "dist",
   clean: true,
+  minify: false,
+  external: ["react", "react-native"],
 });
